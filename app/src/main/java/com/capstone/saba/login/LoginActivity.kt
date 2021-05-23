248ba065bde46e8f647021096434285ea0629786
dayens <dionsunardi1999@gmail.com>
2021-05-24 04:36:12 +0700

    update Layout Login

---
+++ app/src/main/java/com/capstone/saba/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.saba.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.saba.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
