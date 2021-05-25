cbf117bb6a031e1daf7b7272a2cbb8bffdd3ce7a
dayens <dionsunardi1999@gmail.com>
2021-05-25 23:26:11 +0700

    update

---
+++ app/src/main/java/com/capstone/saba/ui/signup/DaftarActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.saba.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.saba.R
+
+class DaftarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar)
+    }
+}
